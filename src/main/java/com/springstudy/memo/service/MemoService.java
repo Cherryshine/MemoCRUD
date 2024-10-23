6e04cc9394fbbc04af88acaff3c9a9a8de9ff7b2
Cherryshine <jee020205@gmail.com>
2024-10-23 20:43:43 +0900

    검색기능 추가

--- src/main/java/com/springstudy/memo/service/MemoService.java
+++ src/main/java/com/springstudy/memo/service/MemoService.java
@@ -5,7 +5,6 @@ import com.springstudy.memo.dto.MemoResponseDto;
 import com.springstudy.memo.entity.Memo;
 import com.springstudy.memo.repository.MemoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -39,6 +38,11 @@ public class MemoService {
     public List<MemoResponseDto> getMemos() {
         return memoRepository.findAllByOrderByModifiedAtDesc().stream().map(MemoResponseDto::new).toList();
     }
+
+    public List<MemoResponseDto> getMemosByKeyword(String keyword) {
+        return (memoRepository.findAllByContentsContainsOrderByModifiedAtDesc(keyword).stream().map(MemoResponseDto::new).toList());
+    }
+
     @Transactional
     public Long updateMemo(Long id, MemoRequestDto requestDto) {
         // 해당 메모가 DB에 존재하는지 확인
@@ -59,5 +63,6 @@ public class MemoService {
         return memoRepository.findById(id).orElseThrow(() ->
                 new IllegalArgumentException("선택한 메모는 존재하지 않습니다."));
     }
+
 
 }
